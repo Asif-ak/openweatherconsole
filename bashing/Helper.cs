43e6a6b77f8bb696c2c40f4804f8c02295db6474
Asif-ak <iu.asifak@gmail.com>
2019-02-24 16:15:34 +0500

    Open Weather Api Console App using curl in linux

---
+++ bashing/Helper.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+
+namespace bashing
+{
+    public static class Helper
+    {
+        public static string Bashing(this string command)
+        {
+            //var espchars = command.Replace("\"", "\\\"");
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "curl", Arguments = $"/-X /-s \"POST\" \"{command}\"", RedirectStandardOutput = true,
+                    UseShellExecute = false, CreateNoWindow = true
+                }
+            };
+            process.Start();
+            var result = process.StandardOutput.ReadToEnd();
+            return result;
+        }
+    }
+}
---
+++ bashing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace bashing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please enter City name: ");
+                var input = Console.ReadLine();
+                if(input=="exit") break;
+                else
+                {
+                    Console.WriteLine($"http://api.openweathermap.org/data/2.5/weather?q={input}&APPID=19d6660a63ed6e4d36b2d6fdd9237042".Bashing());
+                    Console.Read();
+                }
+                break;
+                
+            }
+        }
+        
+    }
+}
