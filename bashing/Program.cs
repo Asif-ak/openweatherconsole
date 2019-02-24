c04f24927363e628ac95ac7676b6af3f842ec463
Asif-ak <iu.asifak@gmail.com>
2019-02-24 17:05:10 +0500

    Open Weather Api Console App using curl in linux update2

--- bashing/Program.cs
+++ bashing/Program.cs
@@ -9,21 +9,25 @@ namespace bashing
     {
         static void Main(string[] args)
         {
-            var input = string.Empty;
-            var link =
-                $"http://api.openweathermap.org/data/2.5/weather?q={input}&APPID=19d6660a63ed6e4d36b2d6fdd9237042";
-            Console.WriteLine("Please enter City name or type 'exit' to close: ");
-            input = Console.ReadLine();
+            while (true)
+            {
+                var input = string.Empty;
+                
+                Console.WriteLine("Please enter City name or type 'exit' to close: ");
+                input = Console.ReadLine();
             
-            if (input == "exit")
-            {
-                Environment.Exit(0);
-            }
-            else
-            {
-                Console.WriteLine(link.Bashing());
-                Console.WriteLine("\n");
-                Console.WriteLine("Do u want to continue[y/n]");
+                if (input == "exit")
+                {
+                    Environment.Exit(0); 
+                }
+                else
+                {
+                    Console.WriteLine($"http://api.openweathermap.org/data/2.5/weather?q={input}&APPID=19d6660a63ed6e4d36b2d6fdd9237042".Bashing());
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Do u want to continue[y/n]");
+                    if(Console.ReadLine()=="n") break;
+                
+                }
                 
             }
 
